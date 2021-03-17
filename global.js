100533fadb72c33780728664f0649fc3358db5d9
beauverger <beauverger.rodolphe@orange.fr>
2021-03-17 11:26:13 +0100

    modif style

--- global.js
+++ global.js
@@ -1,15 +1,1 @@
-/* async function fetchApi(url) {
-  try {
-    let response = await fetch(url);
-    if (response.ok) {
-      let data = await response.json();
-      return data;
-    } else {
-      console.error("retour du serveur : ", response.status);
-    }
-  } catch (err) {
-    console.log(err);
-  }
-} */
-
 const formatCurrency = (elt) => new Intl.NumberFormat("fr-FR", { style: "currency", currency: "EUR" }).format(elt);
