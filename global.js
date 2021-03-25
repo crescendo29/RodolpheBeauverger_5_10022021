125e39edcb688c480b23ade81a92e686fefa3a72
beauverger <beauverger.rodolphe@orange.fr>
2021-03-25 08:22:26 +0100

    modif pattern

--- global.js
+++ global.js
@@ -3,7 +3,8 @@ const formatCurrency = (elt) => new Intl.NumberFormat("fr-FR", { style: "currenc
 // affiche dans le nombre d'articles dans le panier sur chaque page
 const productsInLocalStorage = JSON.parse(localStorage.getItem("inCart"));
 let howManyArticleInCart = 0;
-productsInLocalStorage.forEach((element) => {
+productsInLocalStorage?.forEach((element) => {
   howManyArticleInCart += element.features.quantity;
 });
-document.querySelector(".cart-content").textContent = `Panier(${howManyArticleInCart} articles)`;
+let cartContent = document.querySelectorAll(".cart-content");
+cartContent.forEach((element) => (element.textContent = `Panier(${howManyArticleInCart} articles)`));
